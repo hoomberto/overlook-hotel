4586bd5fa706b9df4f02b9b5c3ea6ef3d6f5a0b4
Bobby Vasquez <shootbv@gmail.com>
2021-06-10 10:17:25 -0600

    Add Hotel class

---
+++ src/Hotel.js
@@ -0,0 +1,13 @@
+class Hotel {
+  constructor() {
+    this.customers = [];
+    this.staff = [];
+    this.rooms = [];
+    this.bookings = [];
+    this.calendar;
+  }
+}
+
+// Hotel should hold onto all data pulled into API, will serve as repo
+
+export default Hotel;
