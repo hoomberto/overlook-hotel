54c2823deba2ffe092d16362f8c7a3d003554ab7
Bobby Vasquez <shootbv@gmail.com>
2021-06-10 10:21:06 -0600

    Add placeholder for checkAvailability method in Hotel.js

--- src/Hotel.js
+++ src/Hotel.js
@@ -6,6 +6,12 @@ class Hotel {
     this.bookings = [];
     this.calendar;
   }
+
+  checkAvailability() {
+    // Go through calendar, check if any dates
+    // Correspond to dates held in bookings
+    // If so, block off those dates
+  }
 }
 
 // Hotel should hold onto all data pulled into API, will serve as repo
