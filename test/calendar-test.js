4463063b2111db712bda804f7988a5491a0f1e7d
Bobby Vasquez <shootbv@gmail.com>
2021-06-11 17:53:18 -0600

    Add test to verify an instance of Calendar will hold the current date

--- test/calendar-test.js
+++ test/calendar-test.js
@@ -1,13 +1,15 @@
 import chai from 'chai';
+import dayjs from 'dayjs';
 // import User from '../src/User'
 // import Hotel from '../src/Hotel'
 import Calendar from '../src/Calendar'
 const expect = chai.expect;
 
 describe('User', () => {
-  let calendar;
+  let calendar, currentDate;
   beforeEach('Setup', () => {
     calendar = new Calendar();
+    currentDate = dayjs().format('YYYY/MM/DD');
   })
   it('should be a function', () => {
     expect(Calendar).to.be.a('function');
@@ -16,6 +18,11 @@ describe('User', () => {
   it('should be an instance of User', () => {
     expect(calendar).to.be.an.instanceof(Calendar);
   });
+
+  it('should hold the current date', () => {
+    expect(calendar.currentDate).to.equal(currentDate);
+  });
+
 
 
 });
