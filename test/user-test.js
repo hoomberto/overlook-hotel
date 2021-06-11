8775318395190d097dcb30e7607593c798e26e1e
Bobby Vasquez <shootbv@gmail.com>
2021-06-11 15:00:22 -0600

    Add basic tests + setup for User class

---
+++ test/user-test.js
@@ -0,0 +1,95 @@
+import chai from 'chai';
+import User from '../src/User'
+import Hotel from '../src/Hotel'
+import Calendar from '../src/Calendar'
+const expect = chai.expect;
+
+describe('User', () => {
+  let user, user2, hotel, calendar;
+  beforeEach('Setup', () => {
+
+    let rooms = [
+      {
+        "number": 1,
+        "roomType": "residential suite",
+        "bidet": true,
+        "bedSize": "queen",
+        "numBeds": 1,
+        "costPerNight": 358.4
+      },
+      {
+        "number": 2,
+        "roomType": "single room",
+        "bidet": false,
+        "bedSize": "full",
+        "numBeds": 2,
+        "costPerNight": 477.38
+      },
+      {
+        "number": 3,
+        "roomType": "single room",
+        "bidet": false,
+        "bedSize": "king",
+        "numBeds": 1,
+        "costPerNight": 491.14
+      }
+    ]
+
+    let bookings = [
+      {
+        "id": "5fwrgu4i7k55hl6sz",
+        "userID": 1,
+        "date": "2020/04/22",
+        "roomNumber": 3,
+        "roomServiceCharges": []
+      },
+      {
+        "id": "5fwrgu4i7k55hl6t5",
+        "userID": 1,
+        "date": "2020/01/24",
+        "roomNumber": 2,
+        "roomServiceCharges": []
+      },
+      {
+        "id": "5fwrgu4i7k55hl6t6",
+        "userID": 2,
+        "date": "2020/01/10",
+        "roomNumber": 1,
+        "roomServiceCharges": []
+      }
+    ]
+
+    let userData = {
+      id: 1,
+      name: 'Nerdo',
+    }
+
+    let userData2 = {
+      id: 2,
+      name: 'Nerdette',
+    }
+
+    user = new User(userData)
+    user2 = new User(userData2)
+    calendar = new Calendar();
+    let users = [user, user2];
+
+    hotel = new Hotel(users, rooms, bookings, calendar)
+
+  })
+  it('should be a function', () => {
+    expect(User).to.be.a('function');
+  });
+
+  it('should be an instance of User', () => {
+    expect(user).to.be.an.instanceof(User);
+  });
+
+  it('should have an ID', () => {
+    expect(user.id).to.equal(1);
+  });
+
+  it('should have a name', () => {
+    expect(user.name).to.equal('Nerdo');
+  });
+});
